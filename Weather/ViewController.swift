de96aa77fffaccae6226d754f5d9fbbdc8dbb04d
bigdub3298 <worldwidewes@wesleys-macbook-pro.local>
2016-09-10 15:50:53 -0600

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Wesley Austin on 9/10/16.
+//  Copyright © 2016 Wesley Austin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
